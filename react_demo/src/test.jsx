118af710cdd9f32f7b7283537e67d2c1bfb71273
yuyang <yuyang@mfexcel.com>
2020-09-15 17:19:25 +0800

    add

--- react_demo/src/test.jsx
+++ react_demo/src/test.jsx
@@ -1,13 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class test extends Component {
   a = this.setState;
 
   render() {
-    return (
-      <div>
-        
-      </div>
-    )
+    return <div></div>;
   }
 }
